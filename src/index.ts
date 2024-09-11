f1cf422103319fa94640947b77b2646c96f0aebe
Vusal Huseynov <vslh333222111@gmail.com>
2024-09-11 20:05:36 +0400

    first commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(3000, () => {
+  console.log(`Server is running on PORT: ${PORT}`);
+});
